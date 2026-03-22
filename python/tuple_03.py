745b71b071f54add85e1dbbecb50f9f593602eeb
Uday Vairalkar <115350165+UdayVairalkar@users.noreply.github.com>
2026-03-22 11:39:08 +0530

    Update tuple_03.py

--- python/tuple_03.py
+++ python/tuple_03.py
@@ -5,9 +5,5 @@ list.sort()
 print(list)
 
 
-"""in this we take a 
-grade and print that how many student 
-score or get grade a 
-and ek list mai grade lie and 
-usnko sort kar k print bhi kia 
+"""  tuple 
 """
